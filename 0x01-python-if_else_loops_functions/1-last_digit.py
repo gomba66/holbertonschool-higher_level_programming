aec81f2e8c6655dfcc8fa4776f6193d22df26ee8
gomba66 <danielalejo66@hotmail.com>
2019-09-10 18:48:55 +0000

    task 1: i fixed a letter

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of", number, end=" ")
+print("Last digit of", number, "is", end=" ")
 if number > 0:
     number = number % 10
 else:
     number = number % -10
-
 if number == 0:
-    print("is", number, "and is 0")
+    print(number, "and is 0")
 elif number > 5:
-    print("is", number, "and is greather than 5")
+    print(number, "and is greater than 5")
 else:
-    print("is", number, "and is less than 6 and not 0")
+    print(number, "and is less than 6 and not 0")
