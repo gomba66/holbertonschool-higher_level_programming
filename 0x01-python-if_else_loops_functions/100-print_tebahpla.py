607f44033a58aa450918fc0e8e991570f08e1be8
gomba66 <danielalejo66@hotmail.com>
2019-09-11 05:10:11 +0000

    New test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 1:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+for i in "zYxWvUtSrQpOnMlKjIhGfEdCbA":
+    print("{}".format(i), end="")
