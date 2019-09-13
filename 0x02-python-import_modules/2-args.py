d58549d90f4c9d5088c41f7bc6c0aee9cfd17804
gomba66 <danielalejo66@hotmail.com>
2019-09-13 02:49:53 +0000

    I fixed the pep8 syntax

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,8 @@
 if __name__ == "__main__":
     import sys
     a = 1
-    print((len(sys.argv) - 1),"arguments:")
+    print((len(sys.argv) - 1), "arguments:")
     for i in sys.argv:
         if i != sys.argv[0]:
-
-
             print(a, ":", i)
             a = a + 1
