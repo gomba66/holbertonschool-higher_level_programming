a96691a55b81eef05ff7a554b41a1ea4d76f679a
gomba66 <danielalejo66@hotmail.com>
2019-09-17 18:20:06 +0000

    I stored the len in a variable

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
+    largo = len(my_list)
+    if idx > largo:
         return (None)
     elif idx < 0:
         return (None)
