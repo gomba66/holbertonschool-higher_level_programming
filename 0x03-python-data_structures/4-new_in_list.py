380d95e449e6e9cd432a3369a249798b2d519e32
gomba66 <danielalejo66@hotmail.com>
2019-09-17 23:29:11 +0000

    Fix the way of i make the copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copylist = []
-    x = 0
-    for i in my_list:
-        x = x + 1
-        copylist.append(i)
+    if my_list == None:
+        return
+    list = my_list.copy()
     if idx < 0:
-        return copylist
-    elif idx > len(my_list):
-        return copylist
-    else:
-        copylist[idx] = element
-        return copylist
+        return list
+    if idx > len(my_list) - 1:
+        return list
+    list[idx] = element
+    return list
