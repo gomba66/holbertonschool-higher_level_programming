1b67124db39722443ba109afa4f9d34938a70381
gomba66 <danielalejo66@hotmail.com>
2019-09-17 19:44:37 +0000

    I fixed the pep8 Syntax

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-
-
     if idx < 0:
         return (None)
-    
     largo = len(my_list)
-    
     if idx > largo - 1:
         return (None)
-
     return my_list[idx]
