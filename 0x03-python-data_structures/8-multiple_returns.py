be9c21b9768e9d16cd217c65e21822bb68fb31a3
gomba66 <danielalejo66@hotmail.com>
2019-10-04 18:32:21 +0000

    I added a new program that returns a tuple with two values

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        len_tot = len(sentence)
+        firts = None
+    else:
+        len_tot = len(sentence)
+        first = sentence[0]
+    return (len_tot, first)
