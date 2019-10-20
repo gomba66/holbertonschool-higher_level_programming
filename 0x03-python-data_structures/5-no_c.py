fb39de7a4deec8cd061060c08d8d5c1da86ee189
gomba66 <danielalejo66@hotmail.com>
2019-10-20 23:11:04 +0000

    I changed the way for to replace letters in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new = new + i
-    return new
+    my_string = my_string.replace("c", "")
+    my_string = my_string.replace("C", "")
+    return my_string
