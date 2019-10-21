958987c474ff73deb6654b61abf442023a4ec73c
gomba66 <danielalejo66@hotmail.com>
2019-10-21 06:51:25 +0000

    I fixed the condition for errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string is not str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     M, D, C, L, X, V, I = 1000, 500, 100, 50, 10, 5, 1
     total2 = 0
