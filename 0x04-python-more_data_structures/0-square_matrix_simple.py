1cf2f63070b70a3bf0f965fa30ed8bc0d8dbb278
gomba66 <danielalejo66@hotmail.com>
2019-10-21 01:27:50 +0000

    I changed the way for the create the new matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return
-    new_matrix = []
-    for x in matrix:
-        ele = list(map((lambda x: x ** 2), x))
-        new_matrix.append(ele)
-    return new_matrix
+    newmtrx = matrix.copy()
+    for i in range(len(matrix)):
+        newmtrx[i] = list(map(lambda x: x**2, matrix[i]))
+    return newmtrx
