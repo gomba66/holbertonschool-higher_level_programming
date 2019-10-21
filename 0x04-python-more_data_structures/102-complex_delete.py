1ddfb7583e5173d46c1bc2f89f16f5f444a4c0da
gomba66 <danielalejo66@hotmail.com>
2019-10-21 14:27:01 +0000

    I added a new function that delete a key using the value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+def complex_delete(a_dictionary, value):
+    k = list(a_dictionary.keys())
+    for index in k:
+        if a_dictionary[index] == value:
+            del a_dictionary[index]
+    return a_dictionary
