e8b3528be902a2c9328d8441ad04117addd639dc
gomba66 <danielalejo66@hotmail.com>
2019-10-22 04:53:02 +0000

    I added a new class with decorators like property and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,4 +9,17 @@ class Square:
             self.__size = size
 
     def area(self):
-        return self.__size**2
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
