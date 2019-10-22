8f205d84ec827ba3c00ee6e7b872bfebdfb05a7e
gomba66 <danielalejo66@hotmail.com>
2019-10-22 04:29:17 +0000

    I deleted a innecesary line

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,6 @@ class MagicClass:
     def __init__(self, radius=0):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius
 
     def area(self):
         return ((self.__radius ** 2) * math.pi)
