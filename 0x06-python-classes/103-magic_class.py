98e3b848826ba2dfe5f602a58b6e4978112b5f4a
gomba66 <danielalejo66@hotmail.com>
2019-10-22 04:23:55 +0000

    I resolve the magic calculation 3

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-print("Empty")
+import math
+class MagicClass:
+    def __init__(self, radius=0):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+    def area(self):
+        return ((self.__radius ** 2) * math.pi)
+    def circunference(self):
+        return (2 * math.pi * self.__radius)
