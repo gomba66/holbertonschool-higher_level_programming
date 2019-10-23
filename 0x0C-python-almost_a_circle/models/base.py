bf24a47343535e86187e62000b016f3fa0690321
gomba66 <danielalejo66@hotmail.com>
2019-10-23 03:52:56 +0000

    I added descriptions to my file base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""" This module contains a class called Base. """
+
+
 class Base:
+    """ This is the base class. """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Special method that inicialize all atributes """
         if id is None:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = self.__nb_objects
