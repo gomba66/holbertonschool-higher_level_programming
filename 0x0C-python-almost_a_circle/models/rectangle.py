a8619ef3e6aa1e2bc21b88bc0b4bc352f28a851d
gomba66 <danielalejo66@hotmail.com>
2019-10-23 04:59:11 +0000

    I added a new class called rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module contains a new class called Rectangle """
+
+from base import Base
+
+
+class Rectangle(Base):
+    """ This is a class Rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Inicialize all atributes """
+
+        pass
