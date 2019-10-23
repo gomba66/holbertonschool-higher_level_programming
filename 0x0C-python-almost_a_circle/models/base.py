4ae6dbe60504f7f31f535896f64d96e51f4d23c9
gomba66 <danielalejo66@hotmail.com>
2019-10-23 03:34:56 +0000

    [Fix] I fixed the pep8 Style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id == None:
+        if id is None:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = self.__nb_objects
         else:
