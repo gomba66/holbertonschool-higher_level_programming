4308bdf894182f6dcfc15f024729b2667acefd9c
gomba66 <danielalejo66@hotmail.com>
2019-10-23 05:28:32 +0000

    I added new methods for the class Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,4 +10,48 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Inicialize all atributes """
 
-        pass
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Method width for retrieve the value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Method that sets a value to width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Method height for retrieve the value """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Method that sets a value to height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Method for retrieve the value """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Method that sets a value to x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Method for retrieve the value """
+        return self.__x
+
+    @y.setter
+    def y(self, value):
+        """ Method that sets a value to y """
+        self.__y = value
