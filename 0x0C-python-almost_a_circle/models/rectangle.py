6ec5c587a1c4226d5dfd3acdaba07cef756e07e4
gomba66 <danielalejo66@hotmail.com>
2019-10-23 06:29:09 +0000

    [task 3]I added the validations for the inputs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,10 @@ class Rectangle(Base):
     def width(self, value):
         """ Method that sets a value to width """
 
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -38,6 +42,10 @@ class Rectangle(Base):
     def height(self, value):
         """ Method that sets a value to height """
 
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     def x(self, value):
         """ Method that sets a value to x """
 
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -62,4 +74,8 @@ class Rectangle(Base):
     def y(self, value):
         """ Method that sets a value to y """
 
+        if type(value) != int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
