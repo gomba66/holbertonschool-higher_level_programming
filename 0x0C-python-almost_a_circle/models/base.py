e8ed8240f565cf9db78a262125f7d2b5cb75e667
gomba66 <danielalejo66@hotmail.com>
2019-10-23 03:32:42 +0000

    I added a Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id == None:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
