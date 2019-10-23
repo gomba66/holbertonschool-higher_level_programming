2398621a83b0804bce36899bea116161e70ec3b5
gomba66 <danielalejo66@hotmail.com>
2019-10-23 05:51:54 +0000

    I added new lines after descriptions of methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,39 +19,47 @@ class Rectangle(Base):
     @property
     def width(self):
         """ Method width for retrieve the value """
+
         return self.__width
 
     @width.setter
     def width(self, value):
         """ Method that sets a value to width """
+
         self.__width = value
 
     @property
     def height(self):
         """ Method height for retrieve the value """
+
         return self.__height
 
     @height.setter
     def height(self, value):
         """ Method that sets a value to height """
+
         self.__height = value
 
     @property
     def x(self):
         """ Method for retrieve the value """
+
         return self.__x
 
     @x.setter
     def x(self, value):
         """ Method that sets a value to x """
+
         self.__x = value
 
     @property
     def y(self):
         """ Method for retrieve the value """
+
         return self.__x
 
     @y.setter
     def y(self, value):
         """ Method that sets a value to y """
+
         self.__y = value
