2794e48cbb5d14ad6213bc22b5ac483fceea163a
gomba66 <danielalejo66@hotmail.com>
2019-10-23 00:07:06 +0000

    [task 0] I added the files of the first task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+This module has a function that adds two
+integer numbers and returns the result.
+
+"""
 def add_integer(a, b=98):
+    """
+    Function that adds two integers.
+
+    Args:
+        a: First integer.
+        b: Second integer.
+    Return:
+        The addition of the two numbers.
+        If the function receive a parameter
+        that is not a integer raise a TypeError
+    """
     if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
