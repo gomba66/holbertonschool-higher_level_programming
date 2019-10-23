fe3ae4798e492c7d127c056a6067cf54f53621c6
gomba66 <danielalejo66@hotmail.com>
2019-10-23 00:58:31 +0000

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,10 +16,8 @@ def add_integer(a, b=98):
         If the function receive a parameter
         that is not a integer raise a TypeError
     """
-    if type(a) != int and type(a) != float:
-        raise TypeError('a must be an integer')
-    if type(b) != int and type(b) != float:
-        raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
