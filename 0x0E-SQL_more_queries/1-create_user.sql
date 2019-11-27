169ae71c1391ea3f9fadbf97c244700aee8b49aa
Gomba66 <danielalejo66@hotmail.com>
2019-11-27 03:48:54 +0000

    I fixed something

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- This script create a new user in the database, and set a password.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
