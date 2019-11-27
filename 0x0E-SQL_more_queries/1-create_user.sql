818f29014572cebbd23afdd6346644aab2473d82
Gomba66 <danielalejo66@hotmail.com>
2019-11-27 03:43:28 +0000

    I added a new query

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- This script create a new user in the database, and set a password.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
