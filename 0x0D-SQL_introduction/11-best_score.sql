d204f89fdb0748bd2f1a1731be5823a179418e2b
Gomba66 <danielalejo66@hotmail.com>
2019-12-16 20:23:52 +0000

    I fixed the syntax for a better readability

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,5 @@
 -- This script shows data of a table that shows a filter output data
-SELECT score, name FROM second_table WHERE score >= 10
+SELECT score, name
+FROM second_table
+WHERE score >= 10
 ORDER BY score DESC;
