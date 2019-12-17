d83f61e4f8435d11cb9221895f4817c28f575e41
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 22:22:46 +0000

    I added a new query

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
--- asdasd
+-- Script that list all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT title, genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
