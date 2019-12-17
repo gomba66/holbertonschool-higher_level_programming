0bd558e893f003bd786b56b348f7394d9f3d6ff5
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 16:30:27 +0000

    I added a new query

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,7 @@
--- asdasd
+-- Script that creates a database called hbtn_0d_usa and a table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+       id INT UNIQUE AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
+);
