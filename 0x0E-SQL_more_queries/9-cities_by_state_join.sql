b7f2c74d62ac326bec4a4a600a06865d7f661d59
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 17:43:49 +0000

    I changed the order of the parameters of the join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Script that list all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON states.id = cities.state_id;
+FROM states
+INNER JOIN cities ON states.id = cities.state_id;
