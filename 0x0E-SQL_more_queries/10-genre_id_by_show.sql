4452423df81c7ba62b48cbbe73cac886cd9e428a
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 21:30:18 +0000

    I added a new query

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,4 @@
--- asdasd
+-- Script that list all shows contained in hbtn_0d_tvshows.
+SELECT title, genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
