46ef5a56d0a7bfa82d5077184abb39ccd0fb9a5e
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 16:24:44 +0000

    I fixed the syntax

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table unique_id with a specific structure.
 CREATE TABLE IF NOT EXISTS unique_id (
-       id INT DEFAULT 1 UNIQUE,
+       id INT UNIQUE DEFAULT 1,
        name VARCHAR(256)
 );
