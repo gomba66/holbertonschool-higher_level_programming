923d5572df58c55e6aeefa8e5fdbe3808b721b62
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 22:08:14 +0000

    I added a new query

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,6 @@
--- asdasdasd
+-- Script that list all shows contained in the database with an specific filter.
+SELECT title, genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
