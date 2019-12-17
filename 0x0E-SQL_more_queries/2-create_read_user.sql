0ad84c41e561cd0051e21b84495fae2c82e80003
Gomba66 <danielalejo66@hotmail.com>
2019-12-17 15:45:49 +0000

    I added a new task for to create a new database and a new user with just a permission

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,6 @@
--- TEST
+-- Script that create a database and a user and give specific privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = PASSWORD('user_0d_2_pwd');
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
