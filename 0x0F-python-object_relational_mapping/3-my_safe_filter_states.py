5f810f3788634f121cba9127db3f6cffcea2586e
Gomba66 <danielalejo66@hotmail.com>
2019-12-18 22:47:01 +0000

    fixx

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id\
-    ASC;", (sys.argv[4],))
+    cur.execute("SELECT * FROM states \
+    WHERE name LIKE %s ORDER BY id ASC;", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
