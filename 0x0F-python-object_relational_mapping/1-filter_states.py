bb3ceeed1d9633e60a6252476cff01a3fd452386
Gomba66 <danielalejo66@hotmail.com>
2019-12-18 20:52:23 +0000

    I fixed the condition

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'n' or row[1][0] == 'N':
+        if row[1][0] == 'N':
             print(row)
     cur.close()
     db.close()
