421a1ccafe98fc42e67bf9258fe875ed2d84776c
Gomba66 <danielalejo66@hotmail.com>
2019-12-18 22:39:02 +0000

    I fixed the syntax

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE\
-    '{}' ORDER BY id ASC"
-                .format(sys.argv[4],))
+    {} ORDER BY id ASC".format(sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
