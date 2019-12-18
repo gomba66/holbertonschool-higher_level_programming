bcf8b1e4d99ff4b0ebd111d990ff09f1566c824e
Gomba66 <danielalejo66@hotmail.com>
2019-12-18 20:50:58 +0000

    I added a new query in python

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains a code for to connect to a database and ask for query
+"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'n' or row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
