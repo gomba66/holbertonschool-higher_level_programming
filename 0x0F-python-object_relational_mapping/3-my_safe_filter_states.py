26f0a8de73b11715deac65db834b77b45b6ba5c3
Gomba66 <danielalejo66@hotmail.com>
2019-12-18 22:44:49 +0000

    I fixed the code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,14 @@ This module contains a code for to connect to a database and ask for query
 import sys
 import MySQLdb
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE\
-    %s ORDER BY id ASC;", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id\
+    ASC;", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
