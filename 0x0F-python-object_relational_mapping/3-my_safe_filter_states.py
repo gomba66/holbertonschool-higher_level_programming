053ce8cb8d11b5c323eb4d02256bfde9e3c6db92
Gomba66 <danielalejo66@hotmail.com>
2019-12-19 00:25:52 +0000

    MEGA FIX

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,21 +4,22 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
+    # Initializes method connect to the database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states\
+    # Return the cursor
+    cursor = db.cursor()
+    # Execute the query and return to the cursor also prevent sql inj
+    cursor.execute("SELECT * FROM states\
     WHERE name LIKE %s ORDER BY id ASC;", (sys.argv[4],))
-
-    query = cur.fetchall()
-
-    for row in query:
-        print(row)
+    # Store the column of the tables
+    tables = cursor.fetchall()
+    # Print the column of the tables
+    for column in tables:
+        print(column)
 
     cursor.close()
     db.close()
