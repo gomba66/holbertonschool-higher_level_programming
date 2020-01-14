a61cc014a52f625f3b23aed09714515818bd91c9
gomba66 <danielalejo66@hotmail.com>
2020-01-14 05:02:54 +0000

    Perrada TIme

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,9 +3,8 @@
 This module contains a program that sends
 a POST request to the URL with an email address
 """
-import urllib.parse
-import urllib.request
-from sys import argv
+import requests
+import sys
 
 if __name__ == "__main__":
     pass
