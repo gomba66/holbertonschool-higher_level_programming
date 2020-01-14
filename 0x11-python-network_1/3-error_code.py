50e5cac4085c88d21755ba19f4e2b6e33cc1b175
gomba66 <danielalejo66@hotmail.com>
2020-01-14 13:20:27 +0000

    I added a new task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains a program that sends
-a POST request to the URL with an email address
+This module contains a program that print
+the error messages of the http request
 """
-import urllib.parse
 import urllib.request
-from sys import argv
+import sys
+import urllib.error
 
 if __name__ == "__main__":
-    pass
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as f:
+            html = f.read().decode()
+            print(html)
+    except urllib.error.HTTPError as error:
+        error = error.code
+        print("Error code: {}".format(error))
