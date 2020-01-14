b1a1a3938e16bc13c70c979bdb37759da970ae49
gomba66 <danielalejo66@hotmail.com>
2020-01-14 04:52:27 +0000

    PERRADA TIME

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,4 @@ import urllib.request
 from sys import argv
 
 if __name__ == "__main__":
-
-    email = {'email': argv[2]}
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
-    r = urllib.request.Request(argv[1], data)
-    with urllib.request.urlopen(r) as f:
-        print(str(f.read(), 'utf-8'))
+    pass
