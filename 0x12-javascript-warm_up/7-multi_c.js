7641d6c8a638b4fe8176d0a514eebeeff3b5229e
gomba66 <danielalejo66@hotmail.com>
2020-02-05 02:09:48 +0000

    I added a new task

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv.length < 3) {
+  console.log('Missing number of ocurrences');
+} else {
+  let i = 0;
+  const number = parseInt(process.argv[2]);
+  while (i < number) {
+    console.log('C is fun');
+    i++;
+}
+}
