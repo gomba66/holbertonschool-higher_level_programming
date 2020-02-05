cd7d7b1575241c07b93b2907809c20d5aabe7438
gomba66 <danielalejo66@hotmail.com>
2020-02-05 04:35:04 +0000

    I fixed the outputs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,13 @@ if (process.argv.length < 4) {
 } else {
   let i = 2;
   let x = process.argv[2];
+  let y = x;
   while (i < process.argv.length) {
+    y = x;
     if (parseInt(process.argv[i]) > x) {
       x = parseInt(process.argv[i]);
     }
     i++;
   }
-  console.log(x);
+  console.log(y);
 }
