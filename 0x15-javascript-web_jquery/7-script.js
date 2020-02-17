57c6a6d29f7ed3d70dec8f0fafc9bbc0292206a4
gomba66 <danielalejo66@hotmail.com>
2020-02-17 00:15:38 +0000

    I fixed a little detail

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-  $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) { $('div').text(result.name); });
+  $.getJSON('https://swapi.co/api/people/5/?format=json', function (result) { $('DIV#character').text(result.name); });
 });
