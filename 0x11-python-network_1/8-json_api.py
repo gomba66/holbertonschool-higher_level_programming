058ff2f7303e500b5e5fb5bd8b16fbf18fadf1e5
gomba66 <danielalejo66@hotmail.com>
2020-03-11 18:52:37 +0000

    FIX3

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 """
-This module contains a program that sends
-a POST request to the URL with an email address
+Write a Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+With json api
 """
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-if __name__ == "__main__":
-    pass
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+
+    req = requests.post('http://0.0.0.0:5000/search_user', {'q': q})
+    try:
+        req_dict = req.json()
+        id = req_dict.get('id')
+        name = req_dict.get('name')
+        if len(req_dict) is 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(req_dict.get('id'), req_dict.get('name')))
+    except:
+        print("Not a valid JSON")
